641b737a186a1f079d46b47842651a473e4447d0
pskier <153275518+pskier@users.noreply.github.com>
2024-03-12 19:56:08 +0100

    Update copy_file.h

--- copy_file.h
+++ copy_file.h
@@ -1,18 +1,4 @@
 #include macro.h
 
-void copy_file(){
-FILE* in;
-FILE* out;
-in = fopen("dane.txt", "r");
-out = fopen("wynik.txt", "w");
-char linia[MAX_LINE_LENGTH];
-
-while (fgets(linia, sizeof(linia), in) != NULL) {
-	fputs(linia, out);
-}
-
-fclose(in);
-fclose(out);
-}
-
+FILE* copy_file(const char *file_name) {};
 
