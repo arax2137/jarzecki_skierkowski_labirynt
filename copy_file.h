4d0831f69eeae9483f7dccc0f0803d6cc5778c11
pskier <153275518+pskier@users.noreply.github.com>
2024-03-12 19:49:54 +0100

    dodanie funkcji kopiującej plik wejściowy

--- copy_file.h
+++ copy_file.h
@@ -1,1 +1,18 @@
 #include macro.h
+
+void copy_file(){
+FILE* in;
+FILE* out;
+in = fopen("dane.txt", "r");
+out = fopen("wynik.txt", "w");
+char linia[MAX_LINE_LENGTH];
+
+while (fgets(linia, sizeof(linia), in) != NULL) {
+	fputs(linia, out);
+}
+
+fclose(in);
+fclose(out);
+}
+
+
