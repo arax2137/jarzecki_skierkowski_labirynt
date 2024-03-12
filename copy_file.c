2d8c420b635999475509bf86c9be492f978b4cb5
pskier <153275518+pskier@users.noreply.github.com>
2024-03-12 19:57:56 +0100

    dodanie funkcji zwracającej wskaźnik na plik z rozwiązaniem

---
+++ copy_file.c
@@ -0,0 +1,16 @@
+#include copy_file.h
+
+FILE* copy_file(const char *file_name) {
+	FILE* in;
+	FILE* out;
+	in = fopen(file_name, "r");
+	out = fopen("rozwiazanie.txt", "w");
+	char linia[MAX_LINE_LENGTH];
+
+	while (fgets(linia, sizeof(linia), in) != NULL) {
+		fputs(linia, out);
+	}
+
+	fclose(in);
+	return out;
+}
