a0f495b86db85aec86fb213ddcb4649a353f7c09
Łukasz Jarzęcki <jarzeckil@gmail.com>
2024-04-14 16:39:31 +0200

    ukonczenie implementacji algorytmu

--- algorithm.h
+++ algorithm.h
@@ -15,11 +15,23 @@ oblicza nastepny i poprzedni punkt ruchu
 */
 struct movement points(struct movement mov);
 
+/*
+okresla poczatkowy kierunek 
+*/
+char baseDirection(struct maze m);
+
+
+/*
+definiuje kierunek w którym wychodzimy ze skrzyżowania
+*/
+char chooseExit(FILE* in, struct maze maz, struct movement mov);
+
 
 /*
 
 */
 void alg(FILE* in, struct maze maz);
+
 
 
 
--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include "macro.h"
 #include "fhandling.h"
+#include "algorithm.h"
 
 
 
 
 int main(){
 	
-	char* name = "maze.txt";
+	char* name = "maze1.txt";
 
 	FILE* f = copy_file(name);
 
 	resetPointer(f);
 	struct maze s = mazeData(f);
 	printf("x - %d, y - %d\nstart - %d/%d\nkoniec - %d/%d\n", s.size_x, s.size_y, s.start_x, s.start_y, s.end_x, s.end_y);
-	
+
+
+	alg(f, s);
+
 	
 	//replaceChr(f, 39, 40, 'V', s);
 	//printf("%c", getChr(f, 39, 40, s));
