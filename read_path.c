3b5baac4845b5178280be97661cd78eefe4206da
pskierkowski <153275518+pskier@users.noreply.github.com>
2024-04-16 10:54:42 +0200

    funkcjonaloność odczytywania ścieżki

---
+++ read_path.c
@@ -0,0 +1,134 @@
+#include "read_path.h"
+
+char direction(FILE* in, int x, int y, struct maze m) {
+	if (getChr(in, y, x + 1, m) == '1')
+		return 'r';
+	else if (getChr(in, y, x - 1, m) == '1')
+		return 'l';
+	else if (getChr(in, y + 1, x, m) == '1')
+		return 'd';
+	else if (getChr(in, y - 1, x, m) == '1')
+		return 'u';
+	return 'c';
+}
+
+int last_step(FILE* in, int x, int y, struct maze m) {
+	if (getChr(in, y + 1, x, m) == 'K' || getChr(in, y - 1, x, m) == 'K' || getChr(in, y, x - 1, m) == 'K' || getChr(in, y, x + 1, m) == 'K')
+		return 1;
+	else
+		return 0;
+}
+
+void read_path(FILE* in,struct maze m)
+{
+
+	FILE* out;
+	out = fopen("kroki.txt", "w");
+	char kierunek = baseDirection(m);
+
+	int x = m.start_x;
+	int y = m.start_y;
+	fprintf(out, "START\n");
+	int count_steps = 0;
+	
+	while (!last_step(in,x,y,m))
+	{
+		switch (kierunek) {
+		case 'r':
+			count_steps = 0;
+			while (getChr(in, y, x, m) != 'X' && getChr(in, y, x, m) != '2' && getChr(in, y, x, m) != 'K') {
+				replaceChr(in, y, x, '.', m);
+				count_steps++;
+				frpintf
+				if (direction(in, x, y, m) != 'r' && direction(in, x, y, m) != 'c')
+					break;
+				if (getChr(in, y, x + 1, m) != 'X' && getChr(in, y, x + 1, m) != '2')
+					x++;
+				else
+					break;
+			}
+			kierunek = direction(in, x, y, m);
+			fprintf(out, "FORWARD %i\n", count_steps - 1);
+			switch (kierunek) {
+			case 'u':
+				fprintf(out, "TURN LEFT\n");
+				break;
+			case 'd':
+				fprintf(out, "TURN RIGHT\n");
+				break;
+			}
+			break;
+		case 'l':
+			count_steps = 0;
+			while (getChr(in, y, x, m) != 'X' && getChr(in, y, x, m) != '2' && getChr(in, y, x, m) != 'K') {
+				replaceChr(in, y, x, '.', m);
+				count_steps++;
+				if (direction(in, x, y, m) != 'l' && direction(in, x, y, m) != 'c')
+					break;
+				if (getChr(in, y, x -1, m) != 'X' && getChr(in, y, x - 1, m) != '2')
+					x--;
+				else
+					break;
+			}
+			kierunek = direction(in, x, y, m);
+			fprintf(out, "FORWARD %i\n", count_steps - 1);
+			switch (kierunek) {
+			case 'u':
+				fprintf(out, "TURN RIGHT\n");
+				break;
+			case 'd':
+				fprintf(out, "TURN LEFT\n");
+				break;
+			}
+			break;
+		case 'u':
+			count_steps = 0;
+			while (getChr(in, y, x, m) != 'X' && getChr(in, y, x, m) != '2' && getChr(in, y, x, m) != 'K') {
+				replaceChr(in, y, x, '.', m);
+				count_steps++;
+				if (direction(in, x, y, m) != 'u' && direction(in, x, y, m) != 'c')
+					break;
+				if (getChr(in, y - 1, x, m) != 'X' && getChr(in, y - 1, x, m) != '2')
+					y--;
+				else
+					break;
+			}
+			kierunek = direction(in, x, y, m);
+			fprintf(out, "FORWARD %i\n", count_steps - 1);
+			switch (kierunek) {
+			case 'l':
+				fprintf(out, "TURN RIGHT\n");
+				break;
+			case 'r':
+				fprintf(out, "TURN LEFT\n");
+				break;
+			}
+			break;
+		case 'd':
+			count_steps = 0;
+			while (getChr(in, y, x, m) != 'X' && getChr(in, y, x, m) != '2' && getChr(in, y, x, m) != 'K') {
+				replaceChr(in, y, x, '.', m);
+				count_steps++;
+			if (direction(in, x, y, m) != 'd' && direction(in, x, y, m) != 'c')
+					break;
+				if (getChr(in, y+1, x, m) != 'X' && getChr(in, y+1, x , m) != '2')
+					y++;
+				else
+					break;
+			}
+			kierunek = direction(in, x, y, m);	
+			fprintf(out, "FORWARD %i\n", count_steps - 1);
+			switch (kierunek) {
+			case 'l':
+				fprintf(out, "TURN LEFT\n");
+				break;
+			case 'r':
+				fprintf(out, "TURN RIGHT\n");
+				break;
+			}
+			break;
+		}
+	} 
+	fprintf(out, "STOP");
+	close(out);
+}
