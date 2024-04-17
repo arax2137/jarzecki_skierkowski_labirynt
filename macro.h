a34f3306c19a030a51ba63ee6cedc8cb74285c08
Łukasz Jarzęcki <jarzeckil@gmail.com>
2024-04-17 18:21:55 +0200

    odczyt pliku binarnego

--- macro.h
+++ macro.h
@@ -5,6 +5,14 @@
 #include <string.h>
 #define MAX_LINE_LENGTH 1024
 
+#define SWAP_2(x) ( (((x) & 0xff) << 8) | ((unsigned short)(x) >> 8) )
+#define SWAP_4(x) ( ((x) << 24) | \
+         (((x) << 8) & 0x00ff0000) | \
+         (((x) >> 8) & 0x0000ff00) | \
+         ((x) >> 24) )
+#define FIX_SHORT(x) (*(unsigned short *)&(x) = SWAP_2(*(unsigned short *)&(x)))
+#define FIX_INT(x)   (*(unsigned int *)&(x)   = SWAP_4(*(unsigned int *)&(x)))
+
 
 struct maze {
 	int size_x;
